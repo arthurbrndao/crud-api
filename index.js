2faa1b2471811f2e4aec416c81b0b6a7cfc2a3df
Arthur Brandão <arthurbrdantas@gmail.com>
2023-06-20 14:38:46 -0300

    feat: add JSON middleware support to express

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const router = require('./src/router')
 const SERVER_PORT = process.env.SERVER_PORT
 
 const app = express()
+
 app.listen(SERVER_PORT, () =>
-  console.log('server running on port ' + SERVER_PORT)
+  console.log('Server is running on port ' + SERVER_PORT)
 )
 
+app.use(express.json())
 app.use(router)
