21746398c76ad6e2f705fdee319d1b28f7f8a662
Arthur Brandão <arthurbrdantas@gmail.com>
2023-06-20 18:27:55 -0300

    feat: add typescript support

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export type User = {
+  id?: number
+  name: string
+  login: string
+  password: string
+  email: string
+  phone: string
+  cpf: string
+  birthdate: string
+  motherName: string
+}
