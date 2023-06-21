439151fee8e9367d8c818af08263292a40a68c14
Arthur Brandão <arthurbrdantas@gmail.com>
2023-06-20 21:25:00 -0300

    feat: add cors support

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 require('dotenv').config()
 const express = require('express')
 const router = require('./src/router')
-
-const SERVER_PORT = process.env.SERVER_PORT
+const cors = require('cors')
 
 const app = express()
 
-app.listen(SERVER_PORT, () =>
-  console.log('Server is running on port ' + SERVER_PORT)
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+  })
+)
+
+app.listen(process.env.SERVER_PORT, () =>
+  console.log('Server is running on port ' + process.env.SERVER_PORT)
 )
 
 app.use(express.json())
